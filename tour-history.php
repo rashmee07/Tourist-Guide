0d73e846805909161b3af309f73a6f906ed71303
Kumari Rashmee <122221443+rashmee07@users.noreply.github.com>
2023-05-15 09:52:53 +0530

    Update tour-history.php

--- tour-history.php
+++ tour-history.php
@@ -71,7 +71,7 @@ $error="You can't cancel booking before 24 hours";
 <!-- Custom Theme files -->
 <script src="js/jquery-1.12.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<!--animate-->
+<!---animate--->
 <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
 <script src="js/wow.min.js"></script>
 	<script>
